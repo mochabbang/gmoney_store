b1618106d94c66f96fea7a33aa678514edddaa47
Sungmin Lee <sungmin9036@gmail.com>
2025-03-12 22:45:57 +0900

    아호..ㅜㅜ 잘안됨됨

--- backend/batch/main.py
+++ backend/batch/main.py
@@ -22,37 +22,69 @@ async def fetch(url, params):
 async def main():
     params = {'KEY': os.getenv("ACCESS_KEY"), 'Type': 'json'}
   
-    # 경기도 행정지역 명칭 불러오기  
-    region_url = os.getenv("GGREGION_API_URL")
-    region_response = await fetch(region_url, params)
-    
-    region_rsp_json = json.loads(region_response)
-    
-    region_raw_datas = region_rsp_json.get("GGADMINHIGHGBST")[-1].get("row")
-    regions = [region_raw_data.get("SIGUN_NM") for region_raw_data in region_raw_datas]
-    
-    # 경기도 지역정보 조회
-    gmoney_url = os.getenv("GMONEY_API_URL")
-    params["pIndex"] = 1
-    params["pSize"] = 1
-    
-    # 지역명,가맹점 건 수 수집
-    gmoney_sigunstore_infos = []
-    for region in regions:
+    try:
+        # 경기도 행정지역 명칭 불러오기  
+        region_url = os.getenv("GGREGION_API_URL")
+        region_response = await fetch(region_url, params)
         
-        if region in ["의정부","남양주"]:
-            region = f"{region}시"
+        region_rsp_json = json.loads(region_response)
+        
+        region_raw_datas = region_rsp_json.get("GGADMINHIGHGBST")[-1].get("row")
+        regions = [region_raw_data.get("SIGUN_NM") for region_raw_data in region_raw_datas]
+        
+        # 경기도 지역정보 조회
+        gmoney_url = os.getenv("GMONEY_API_URL")
+        params["pIndex"] = 1
+        params["pSize"] = 1
+        
+        # 지역명,가맹점 건 수 수집
+        gmoney_sigun_infos = []
+        for region in regions:
             
-        params["SIGUN_NM"] = region        
+            if region in ["의정부","남양주"]:
+                region = f"{region}시"
+                
+            params["SIGUN_NM"] = region        
+            
+            gmoney_response = await fetch(gmoney_url, params)
+            gmoney_rsp_json = json.loads(gmoney_response)
         
-        gmoney_response = await fetch(gmoney_url, params)
-        gmoney_rsp_json = json.loads(gmoney_response)
+            storecnt = gmoney_rsp_json.get("RegionMnyFacltStus")[0].get("head")[0].get('list_total_count', 0)
+            # gmoney_sigunstore_info = {} 
+            # gmoney_sigunstore_info[region] = storecnt
+            # gmoney_sigunstore_infos.append(gmoney_sigunstore_info)
+            
+            page_totalcnt = storecnt // 1000
+            
+            for i in range(page_totalcnt):
+                params["pIndex"] = int(i) + 1
+                params["pSize"] = 1000
+                 
+                gmoney_response = await fetch(gmoney_url, params)
+                gmoney_rsp_json = json.loads(gmoney_response)
+                
+                store_list= gmoney_rsp_json.get("RegionMnyFacltStus")[1].get("row")
+                gmoney_sigun_infos.extend(store_list)
+                #print(store_list)
+                
+            reminder_cnt = storecnt - (page_totalcnt * 1000)
+            if reminder_cnt > 0:
+                params["pIndex"] = page_totalcnt + 1
+                params["pSize"] = reminder_cnt
+                
+                gmoney_response = await fetch(gmoney_url, params)
+                gmoney_rsp_json = json.loads(gmoney_response)
+                
+                store_list= gmoney_rsp_json.get("RegionMnyFacltStus")[1].get("row")
+                gmoney_sigun_infos.extend(store_list)
+                
+            asyncio.sleep(5)
+            
+        print(gmoney_sigun_infos)
+            
+    except Exception as e:
+        raise e    
     
-        storecnt = gmoney_rsp_json.get("RegionMnyFacltStus")[0].get("head")[0].get('list_total_count', 0)
-        gmoney_sigunstore_info = {} 
-        gmoney_sigunstore_info[region] = storecnt
-        gmoney_sigunstore_infos.append(gmoney_sigunstore_info)
-        
-    # 지역, 가맹점 비동기 병렬로 1/n나누어 데이터셋 구성
+    
     
 asyncio.run(main())
